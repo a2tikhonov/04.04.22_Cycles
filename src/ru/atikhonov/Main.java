0189394aaf35e742c8e909a0aa0ec1fb114df564
Alexander Tikhonov <a.a.tikhonov@outlook.com>
2022-04-04 19:29:33 +0300

    Сделал домашку

--- src/ru/atikhonov/Main.java
+++ src/ru/atikhonov/Main.java
@@ -3,6 +3,30 @@ package ru.atikhonov;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //Задание 1
+    int i = 1;
+	while(i <= 10) {
+        System.out.print(i + " ");
+        if (i == 10) {
+            System.out.print("\n");
+            break;
+        }
+        i++;
+    }
+    for(; i > 0; i--) {
+        System.out.print(i + " ");
+    }
+    System.out.print("\n");
+    //Задание 2
+    for(i = 1; i <= 31; i += 7) {
+        System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+    }
+    //Задание 3
+    int currentYear = 2022;
+    int leftBoundary = currentYear - 200;
+    int rightBoundary = currentYear + 100;
+    for (i = 0; i <= rightBoundary; i += 79 ) {
+        if (i >= leftBoundary) System.out.println(i);
+    }
     }
 }
